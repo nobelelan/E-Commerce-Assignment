c9887ff7de471e1bc32eee49cd865d0416831456
Nobel Elan <nobelelan@gmail.com>
2023-03-26 03:24:36 +0600

    attempt 4

---
+++ app/src/main/java/com/example/e_commerce/ui/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.e_commerce.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.e_commerce.R
+import com.example.e_commerce.databinding.ActivitySplashBinding
+import com.example.e_commerce.ui.main.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000) // delay time in milliseconds
+    }
+}
