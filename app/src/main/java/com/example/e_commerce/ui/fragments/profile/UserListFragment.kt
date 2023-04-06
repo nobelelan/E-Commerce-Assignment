67770a65fc3c4608c8b7c95129d145a44adecf3f
Nobel Elan <nobelelan@gmail.com>
2023-04-07 05:21:23 +0600

    admin works fine

--- app/src/main/java/com/example/e_commerce/ui/fragments/profile/UserListFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/fragments/profile/UserListFragment.kt
@@ -3,10 +3,12 @@ package com.example.e_commerce.ui.fragments.profile
 import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e_commerce.R
@@ -28,6 +30,10 @@ import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 class UserListFragment : Fragment() {
@@ -60,21 +66,66 @@ class UserListFragment : Fragment() {
 
         userListAdapter.setOnClickListener(object : UserListAdapter.OnItemClickListener{
             override fun onRoleClick(profile: Profile) {
-
+                if (profile.role != "admin"){
+                    showAlertDialog(profile)
+                }else{
+                    requireActivity().showToast("You can't demote an Admin.")
+                }
             }
         })
     }
 
-    private fun showAlertDialog(role: String){
+    private fun showAlertDialog(profile: Profile){
         AlertDialog.Builder(requireContext())
-            .setTitle("Shift Access?")
+            .setTitle("Make Admin?")
             .setNegativeButton("Cancel"){_,_->}
             .setPositiveButton("Yes"){_,_->
+
+                val batch = Firebase.firestore.batch()
+                Firebase.firestore.collectionGroup("profile")
+                    .whereEqualTo("phone", profile.phone)
+                    .get()
+                    .addOnSuccessListener { querySnapshot ->
+                        if (querySnapshot.documents.isNotEmpty()) {
+                            querySnapshot.documents.forEach { documentSnapshot ->
+                                val docRef = Firebase.firestore.collection("users")
+                                    .document(documentSnapshot.reference.parent.parent!!.id)
+                                    .collection("profile").document(documentSnapshot.id)
+                                batch.update(docRef, "role", ROLE_ADMIN)
+                            }
+                            batch.commit()
+                        }
+                    }
 
             }.create().show()
     }
 
     private fun retrieveAndSetUserListData() {
+        val profileList = arrayListOf<Profile>()
+
+        Firebase.firestore.collectionGroup("profile")
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                // Loop through the documents in the "profile" collection group
+                querySnapshot.documents.forEach { documentSnapshot ->
+                    // Convert the document to Profile object
+                    val profile = documentSnapshot.toObject(Profile::class.java)
+                    // Add the profile to the profileList
+                    profile?.let { profileList.add(it) }
+
+                    // Check if this is the last profile document
+                    if (profileList.size == querySnapshot.documents.size) {
+                        // All profiles have been fetched
+                        // You can use profileList here
+                        userListAdapter.differCallBack.submitList(profileList)
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle error if profile collection retrieval fails
+                // You may want to handle this case based on your specific use case
+            }
+
 
     }
 
